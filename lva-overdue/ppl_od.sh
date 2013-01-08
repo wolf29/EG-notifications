1d2e1a8db58a17811db110b8a83d71e3f8562d67
Wolf Halton <wolf.halton@lyrasis.org>
2013-01-08 15:06:28 -0500

    Attempting to get all the files up there.

---
+++ lva-overdue/ppl_od.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# ---------------------------------------------------------------
+# This file runs the overdue generation script.
+# If today is Monday, it runs the script for Sat/Sun/Mon, 
+# otherwise it runs once per day.
+# ---------------------------------------------------------------
+
+
+
+
+SSH_CLIENT=$1
+RECIPIENT=$2;
+DATE=$(date +%Y-%m-%d);
+DAY=$(date +%u);
+BSCONFIG="/openils/conf/opensrf_core.xml"
+ODDIR="/openils/var/data/overdue/ppl";
+
+export EG_OVERDUE_EMAIL_TEMPLATE="overdue_notice_email";
+export EG_OVERDUE_SMTP_HOST="";
+export EG_OVERDUE_EMAIL_SENDER="libnotice@portsmouthva.gov";
+
+[ $(whoami) != "opensrf" ] && echo "Must be run as opensrf" && exit 1;
+source ~/.bashrc;
+ARGS="0"
+
+#[ $DAY == 6 -o $DAY == 7 ] && exit 0; # don't run on saturday or sunday
+[ $DAY == 7 ] && exit 0; # Don't run on Sunday
+#if [ $DAY == 1 ]; then ARGS="2 1 0"; fi; # If today is monday, run for sat/sun/mon
+if [ $DAY == 1 ]; then ARGS="1 0"; fi; # If today is monday, run for sun/mon
+
+
+./ppl_od.pl $BSCONFIG $ARGS > "$ODDIR/ppl.overdue.$DATE.xml"
+
