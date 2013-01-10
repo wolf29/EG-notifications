1b78295da3c92a434c9cf8046fa3b8f1feb2750a
Wolf Halton <wolf.halton@lyrasis.org>
2013-01-10 15:42:37 -0500

    Update of transform and deliver as well a a tweak of the ppl_od.[pl|sh]

---
+++ deliver_ppl.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl 
+
+
+my @date = CORE::localtime;
+my $d    = $date[3];
+my $mon  = $date[4] + 1;
+my $year = $date[5] + 1900;
+
+if ($d < 10){$d="0$d";}
+if ($mon < 10){$mon="0$mon";}
+
+my $day = "$year-$mon-$d";
+my $file= "/openils/var/data/overdue/ppl/ppl.overdue.""${day}""_print.pdf";
+
+## system ("/usr/bin/scp  $file grpl\@www.branchdistrictlibrary.org:");
+## system ("/usr/bin/scp  $file support\@lyrasistechnology.org:/home/support/lva-overdues/ppl");
+system ("/usr/bin/mutt -s "Overdue Notices" -a $file -- robin.dale@lyrasis.org < /home/opensrf/overdue/file_delivery.txt")
+
---
+++ xml_transform.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#====================================
+# This script runs xsltproc to get an xml file that is specifically
+# the print version of the xml data.
+# and runs prince to make that xml file into a PDF
+
+cd /openils/var/data/overdue/ppl
+
+
+myfile=find -maxdepth 1 -name "*.xml" -mmin -20
+
+echo "$myfile is the file we are taking"
+
+slim="`echo $myfile | cut -d '.' -f 2 | cut -d '/' -f 2`"
+
+echo "$slim is the stub"
+
+xsltproc ../overdues.xsl $myfile > "${slim}_print.xml"
+
+prince_pack="${slim}_print.xml"
+
+echo "${prince_pack} is the package for Prince"
+
+prince "${prince_pack}"
+
+
+
